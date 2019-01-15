983982fd972228fab4b172f7dd64bf6395cc91d1
John Barrett <johnjbarrett22@gmail.com>
2019-01-15 10:32:08 -0800

    JSP directory set and controller/service created

--- src/main/java/com/michaels/mini_mart/controllers/MiniMartController.java
+++ src/main/java/com/michaels/mini_mart/controllers/MiniMartController.java
@@ -1,5 +1,23 @@
 package com.michaels.mini_mart.controllers;
 
-public class MiniMartController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.michaels.mini_mart.services.MiniMartService;
+
+
+@Controller
+public class MiniMartController {
+	private final MiniMartService miniMartService;
+	
+	public MiniMartController(MiniMartService miniMartService) {
+		this.miniMartService = miniMartService;
+	}
+
+	//Index
+	@GetMapping("")
+	public String index(Model model) {
+		return "index.jsp";
+	}	
 }
--- src/main/java/com/michaels/mini_mart/services/MiniMartService.java
+++ src/main/java/com/michaels/mini_mart/services/MiniMartService.java
@@ -1,5 +1,8 @@
 package com.michaels.mini_mart.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MiniMartService {
 
 }
